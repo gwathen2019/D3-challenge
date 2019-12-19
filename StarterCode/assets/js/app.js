e35b26e05d5fe28b100d32bd7c2252472e76e863
Greg Wathen <53319385+gwathen2019@users.noreply.github.com>
2019-12-19 16:49:00 -0600

    git commit

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,4 +1,16 @@
 // @TODO: YOUR CODE HERE!
+
+// The code for the chart is wrapped inside a function
+// that automatically resizes the chart
+function makeResponsive() {
+
+  // if the SVG area isn't empty when the browser loads, remove it
+  // and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+
 // Define SVG area dimensions
 var svgWidth = 960;
 var svgHeight = 660;
@@ -7,8 +19,8 @@ var svgHeight = 660;
 var chartMargin = {
   top: 30,
   right: 30,
-  bottom: 30,
-  left: 30
+  bottom: 100,
+  left: 100
 };
 
 var width = svgWidth - chartMargin.left - chartMargin.right;
@@ -43,7 +55,7 @@ d3.csv("/assets/data/data.csv").then(function(acs_Data) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([8, d3.max(acs_Data, d => d.poverty)])
+      .domain([6, d3.max(acs_Data, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
@@ -81,7 +93,7 @@ d3.csv("/assets/data/data.csv").then(function(acs_Data) {
     .enter()
     .append("text")
     .attr("x", d => xLinearScale(d.poverty))
-    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("y", d => yLinearScale(d.healthcare) + 5)
     .attr("text-anchor", "middle")
     .attr("font-family", "sans-serif")
     .attr("font-size", "15px")
@@ -91,22 +103,22 @@ d3.csv("/assets/data/data.csv").then(function(acs_Data) {
   // Append axes titles
     chartGroup.append("text")
     .attr("class", "x label")
-    .attr("text-anchor", "end")
-    .attr("x", width)
-    .attr("y", height - 10)
+    .attr("text-anchor", "middle")
+    .attr("x", width/2)
+    .attr("y", height + 40)
     .text("% in Poverty");
 
     chartGroup.append("text")
     .attr("class", "y label")
-    .attr("text-anchor", "end")
-    .attr("y", 6)
+    .attr("text-anchor", "middle")
+    .attr("y", height/2)
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
     .text("Health Care (%)");
 
-  });
-/*
-    // Step 6: Initialize tool tip
+//  });
+
+/*    // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
       .attr("class", "tooltip")
@@ -136,14 +148,38 @@ d3.csv("/assets/data/data.csv").then(function(acs_Data) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+      .text("Health Care(%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text;
+      .text("% in Poverty");
+*/
+// Step 1: Append a div to the body to create tooltips, assign it a class
+  // =======================================================
+  var toolTip = d3.select("body").append("div")
+    .attr("class", "tooltip");
+
+  // Step 2: Add an onmouseover event to display a tooltip
+  // ========================================================
+  circlesGroup.on("mouseover", function(d, i) {
+    toolTip.style("display", "block");
+    toolTip.html(`% in Poverty: <strong>${d.poverty[i]}</strong>`)
+      .style("left", d3.event.pageX + "px")
+      .style("top", d3.event.pageY + "px");
+  })
+    // Step 3: Add an onmouseout event to make the tooltip invisible
+    .on("mouseout", function() {
+      toolTip.style("display", "none");
+    });
+
 
   }).catch(function(error) {
     console.log(error);
-});
-*/
+  })
+}
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, responsify() is called.
+d3.select(window).on("resize", makeResponsive);
